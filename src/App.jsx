11983b0a303a166ba2af8b445ae06bd43137213d
neoayus <ayus43@outlook.com>
2026-04-08 00:16:45 +0530

    learn: react router

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
 
 export default function App(){
   return(
     <>
-      <h2>this that React Router!</h2>
       <BrowserRouter> 
         <Routes> 
           <Route path="/" element={<Homepage/>} />
           <Route path="product" element={<Product/>} />
           <Route path="pricing" element={<Pricing/>} />
+          {/* if the url dosen't match anything from above  */}
+          <Route path="*" element={<PageNotFound/>} />
         </Routes>
       </BrowserRouter>
       
