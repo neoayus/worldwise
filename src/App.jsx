9dd1ce7b73d2995aa83263bacfa3909ff4d3beb4
neoayus <ayus43@outlook.com>
2026-04-08 00:07:17 +0530

    learn: react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+
 export default function App(){
   return(
-    App.
+    <>
+      <h2>this that React Router!</h2>
+      <BrowserRouter> 
+        <Routes> 
+          <Route path="/" element={<Homepage/>} />
+          <Route path="product" element={<Product/>} />
+          <Route path="pricing" element={<Pricing/>} />
+        </Routes>
+      </BrowserRouter>
+      
+    </>
   )
 }
