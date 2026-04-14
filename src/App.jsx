6c7e55280934b4f8aefd0bb34bd26545f9ba6d01
neoayus <ayus43@outlook.com>
2026-04-14 16:19:47 +0530

    feat: programatic navigation with useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,10 @@ import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import City from "./components/City";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import Form from "./components/Form";
 
 const URL = "http://localhost:8000";
 
@@ -48,11 +50,12 @@ export default function App() {
               path="cities"
               element={<CityList cities={cities} IsLoading={IsLoading} />}
             />
+            <Route path="cities/:id" element={<City />} />
             <Route
               path="countries"
               element={<CountryList cities={cities} isLoading={IsLoading} />}
             />
-            <Route path="form" element={<p> Form </p>} />
+            <Route path="form" element={<Form />} />
           </Route>
           {/* if the url dosen't match anything from above  */}
           <Route path="*" element={<PageNotFound />} />
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,19 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
-export default function Map(){
-  return(
-    <div className={styles.mapContainer}>
+export default function Map() {
+  const navigate = useNavigate();
 
-    </div>
-  )
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}
+    ></div>
+  );
 }
